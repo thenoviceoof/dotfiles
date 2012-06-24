8cbe6c0b892e837a5e5f86c2ba9cb576100fcebf
Nathan Hwang <thenoviceoof@gmail.com>
2012-06-24 02:07:31 -0400

    Added empty workspace finder, still working on adding M-0

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -3,13 +3,22 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Util.EZConfig
 import XMonad.Config.Gnome
 
+import XMonad.Actions.FindEmptyWorkspace
+-- import XMonad.Actions.DynamicWorkspaces
+
 myManageHook = composeAll
                [ className =? "Do" --> doFloat
                , manageDocks
                ]
 
+-- myWorkspaces = map show [1 .. 9]
+
+moreKeys = [ ("M-m", viewEmptyWorkspace)
+           -- , ("M-0", (switchToWorkspace 0))
+           ]
+
 main = xmonad $ gnomeConfig
         { terminal = "gnome-terminal"
         , modMask = mod4Mask
         , manageHook = myManageHook <+> manageHook defaultConfig
-        }
+        } `additionalKeysP` moreKeys
