99c307899ff87d79cac2666c60d4214456c7fa37
Nathan Hwang <thenoviceoof@gmail.com>
2012-06-30 15:44:01 -0400

    Added a script to refresh the dotfiles, b/c hard links don't work

---
+++ refresh.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+################################################################################
+# get all the tracked files
+
+cp ~/.emacs .
+cp ~/.zshrc .
+cp ~/.xmonad/xmonad.hs .xmonad/
