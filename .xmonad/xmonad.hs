2b3dcf5635074d77d0ce72a85f66711402a55c28
Nathan Hwang <thenoviceoof@gmail.com>
2012-06-30 15:25:56 -0400

    Added additional workspaces

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -4,21 +4,26 @@ import XMonad.Util.EZConfig
 import XMonad.Config.Gnome
 
 import XMonad.Actions.FindEmptyWorkspace
--- import XMonad.Actions.DynamicWorkspaces
+import XMonad.Actions.DynamicWorkspaces
+
+import qualified XMonad.StackSet as W
 
 myManageHook = composeAll
                [ className =? "Do" --> doFloat
                , manageDocks
                ]
 
--- myWorkspaces = map show [1 .. 9]
+myWorkspaces = map show [1 .. 12]
 
 moreKeys = [ ("M-m", viewEmptyWorkspace)
-           -- , ("M-0", (switchToWorkspace 0))
+           , ("M-0", windows $ W.greedyView "10")
+           , ("M-[", windows $ W.greedyView "11")
+           , ("M-]", windows $ W.greedyView "12")
            ]
 
 main = xmonad $ gnomeConfig
         { terminal = "gnome-terminal"
         , modMask = mod4Mask
-        , manageHook = myManageHook <+> manageHook defaultConfig
+        , manageHook = myManageHook <+> manageHook gnomeConfig
+        , workspaces = myWorkspaces
         } `additionalKeysP` moreKeys
