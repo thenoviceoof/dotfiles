c24b89a1514a065b87ba670e14b92585cefc99e1
Nathan Hwang <thenoviceoof@gmail.com>
2013-06-02 19:53:05 -0400

    Fix issue #3: use os.urandom() for randomness

--- genpass.py
+++ genpass.py
@@ -3,6 +3,7 @@
 import time
 import base64
 import hashlib
+import os
 
-hash_out = base64.urlsafe_b64encode(hashlib.md5(str(time.time())).digest())
+hash_out = base64.urlsafe_b64encode(hashlib.md5(str(os.urandom(36))).digest())
 print hash_out[:-2].replace('-', ',').replace('_', '.')
