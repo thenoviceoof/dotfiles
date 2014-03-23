b44298eeb9e0f2fedfab56e9f73bec400cf4def9
Nathan Hwang <thenoviceoof@gmail.com>
2014-03-23 00:36:52 -0400

    Switched back to using emacs from emacsclient

--- e.sh
+++ e.sh
@@ -1,8 +1,12 @@
-#!/bin/sh
+#!/bin/bash
+################################################################################
+# switched back to emacs: I usually have long lived emacs windows, and
+# the whole frame system was making me sad:
+#  - git would try to open a new commit window, and that would screw
+#    up old frames
+#  - opening new frames in different directories would not shift the
+#    CWD to the new directory, or would shift the default-directory
+#    for all frames
 
-if [ $1 ]
-then
-	emacsclient -nw --alternate-editor='' -e "(progn (select-frame (make-frame)) (cd \"`pwd`\") (find-file \"$1\"))"
-else
-	emacsclient -nw --alternate-editor='' -e "(progn (select-frame (make-frame)) (cd \"`pwd`\"))"
-fi
+emacs -nw "$@"
+# emacsclient -nw --alternate-editor="" -c "$@"
