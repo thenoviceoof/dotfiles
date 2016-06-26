78aa04b73a1c026d93be050027854a83c7183628
Nathan Hwang <thenoviceoof@gmail.com>
2016-06-26 12:42:37 -0400

    writing: add the writing check script

---
+++ writing-script.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+# Clean up old html files.
+rm -f *.html
+
+for file in $(ls | grep -v html | grep -v \.sh); do
+    echo "############################################################"
+    echo $file
+    echo ""
+
+    tmpfile=$(mktemp)
+    cat $file | perl -0777 -pe 's/\n\n\n.*//igs' >$tmpfile
+
+    # Run proselint.
+    echo "---------- Proselint"
+    proselint $tmpfile
+    echo ""
+
+    ## TODO: add professor scripts here
+
+    # Generate markdown.
+    echo "---------- Generating markdown..."
+    mdfile="$file.html"
+    cat $tmpfile | markdown >$mdfile
+    echo ""
+
+    # Clean up.
+    rm $tmpfile
+done
