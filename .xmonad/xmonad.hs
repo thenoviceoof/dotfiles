a8c2cb383a6dcc1445c4bdb11261c72e29685225
thenoviceoof <thenoviceoof@gmail.com>
2016-10-30 01:13:00 -0400

    xmonad: my god, add some more comments you fool

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -17,6 +17,7 @@ import XMonad.Hooks.ICCCMFocus
 
 
 modkey = mod4Mask
+-- TODO: clean this up
 myManageHook = composeAll
                [ className =? "Do" --> doFloat
                , className =? "Pidgin" --> doShift "12"
@@ -25,18 +26,22 @@ myManageHook = composeAll
                , manageDocks
                ]
 
+-- Extend the desktops past just 1-9
 moreWorkspaces = map show [10 .. 12]
 moreKeys = [xK_0, xK_bracketleft, xK_bracketright]
 myWorkspaces = map show [1 .. 9] ++ moreWorkspaces
 
-myKeys = [ ((modkey, xK_m), viewEmptyWorkspace)
+myKeys = [ -- M-m shows the next empty workspace
+           ((modkey, xK_m), viewEmptyWorkspace)
          ]
          ++
+         -- M-{:digit:} accesses each desktop, +shift moves windows
          [((m .|. modkey, k), windows $ f i)
          | (k, i) <- zip moreKeys moreWorkspaces
          , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
          ]
          
+-- By default, split the last desktop unevenly (pidgin layout)
 myLayouts = onWorkspace "12" imLayout $ layoutHook gnomeConfig
   where
     imLayout = Tall 1 (3/100) (3/4)
