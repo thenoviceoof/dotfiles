0b2ed1fd7bb1dea8cb5785d180ed1caadcf045d0
thenoviceoof <thenoviceoof@gmail.com>
2016-10-30 00:42:34 -0400

    push: add a push script, which pushes the repo's config files out

---
+++ push.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+if [ -e ~/.emacs ]
+then
+    mv ~/.emacs ~/.emacs_bak
+fi
+cp .emacs ~/
+
+cp frisky.zsh-theme ~/.oh-my-zsh/themes/
+
+if [ -e ~/.zshrc ]
+then
+    mv ~/.zshrc ~/.zshrc_bak
+fi
+cp .zshrc ~/
+
+mkdir -p ~/.local/bin
+
+cp e.sh ~/.local/bin/
+if [ ! -e ~/.local/bin/e ]
+then
+    ln -s ~/.local/bin/e.sh ~/.local/bin/e
+fi
+
+cp t.sh ~/.local/bin/
+if [ ! -e ~/.local/bin/t ]
+then
+    ln -s ~/.local/bin/t.sh ~/.local/bin/t
+fi
+
+cp genpass.py ~/.local/bin/
+if [ ! -e ~/.local/bin/genpass ]
+then
+    ln -s ~/.local/bin/genpass.py ~/.local/bin/genpass
+fi
+
+cp -R .xmonad ~/.xmonad
+
+cp .gitignore_global ~/
+git config --global core.excludesfile ~/.gitignore_global
