054940a1e04168890e26f702332f451da97862a1
thenoviceoof <thenoviceoof@gmail.com>
2017-05-27 14:34:03 -0400

    NAS: don't create ssh tunnels if they already exist

--- kalnas_mount.sh
+++ kalnas_mount.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
 # Port for nfsd.
-ssh kalnas -L 3048:localhost:2049 -f -N
+NFSD_TUNNEL=`lsof -i -P -n | grep 127.0.0.1:3048`
+if [ -z "$NFSD_TUNNEL" ]; then
+   ssh kalnas -L 3048:localhost:2049 -f -N
+fi
 # Port for mountd.
-ssh kalnas -L 3049:localhost:32222 -f -N
+NFSD_TUNNEL=`lsof -i -P -n | grep 127.0.0.1:3049`
+if [ -z "$NFSD_TUNNEL" ]; then
+    ssh kalnas -L 3049:localhost:32222 -f -N
+fi
 # Use the ssh tunnels to access the NFS share.
 sudo mount -t nfs -o port=3048,mountport=3049,tcp localhost:/mnt/Data/DataShare ~/d
