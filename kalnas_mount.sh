72e965b8bf6362e7f11dc98960ce8f9660b67ba9
thenoviceoof <thenoviceoof@gmail.com>
2017-05-27 18:22:49 -0400

    NAS: interpolate the autossh command, which mysteriously works better

--- kalnas_mount.sh
+++ kalnas_mount.sh
@@ -2,19 +2,17 @@
 
 set -e
 
-SSHCMD="autossh -M 0 -o 'ServerAliveInterval 5' -o 'ServerAliveCountMax 3'"
-
 # Port for nfsd.
 NFSD_TUNNEL=`lsof -i -P -n | grep 127.0.0.1:3048 || true`
 if [ -z "$NFSD_TUNNEL" ]; then
-    $SSHCMD kalnas -L 3048:localhost:2049 -f
     echo "Starting NFS tunnel..."
+    autossh -M 0 -f -T -N -o "ServerAliveInterval 5" -o "ServerAliveCountMax 3" kalnas -L 3048:localhost:2049
 fi
 # Port for mountd.
 NFSD_TUNNEL=`lsof -i -P -n | grep 127.0.0.1:3049 || true`
 if [ -z "$NFSD_TUNNEL" ]; then
-    $SSHCMD kalnas -L 3049:localhost:32222 -f
     echo "Starting mountd tunnel..."
+    autossh -M 0 -f -T -N -o "ServerAliveInterval 5" -o "ServerAliveCountMax 3" kalnas -L 3049:localhost:32222
 fi
 # Use the ssh tunnels to access the NFS share.
 # Put the mount command in it's own file, so we can run it with sudo.
