0b27942bd7a0a34ce216efa289c0a32bca97d867
thenoviceoof <thenoviceoof@gmail.com>
2017-05-27 17:34:23 -0400

    NAS: use a shorter NFS connection timeout

--- kalnas_mount_command.sh
+++ kalnas_mount_command.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-mount -t nfs -o soft,port=3048,mountport=3049,tcp localhost:/mnt/Data/DataShare ~/d
+# By default, uses 3 requests.
+# Timeout increases 5, 10, 15 (uses deci-seconds as units).
+mount -t nfs -o soft,timeo=50,port=3048,mountport=3049,tcp localhost:/mnt/Data/DataShare ~/d
