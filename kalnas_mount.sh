a697096070ed59d72d71289371c5503cc9c14fa7
thenoviceoof <thenoviceoof@gmail.com>
2017-05-27 14:23:01 -0400

    NAS: don't run the sleep commands, just daemonize

--- kalnas_mount.sh
+++ kalnas_mount.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # Port for nfsd.
-ssh kalnas -L 3048:localhost:2049 -f sleep 60
+ssh kalnas -L 3048:localhost:2049 -f -N
 # Port for mountd.
-ssh kalnas -L 3049:localhost:32222 -f sleep 60
+ssh kalnas -L 3049:localhost:32222 -f -N
 # Use the ssh tunnels to access the NFS share.
 sudo mount -t nfs -o port=3048,mountport=3049,tcp localhost:/mnt/Data/DataShare ~/d
