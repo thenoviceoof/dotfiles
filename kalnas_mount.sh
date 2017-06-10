2afadab9c78d949f075b7e681c10b08f0205a4bc
thenoviceoof <thenoviceoof@gmail.com>
2017-06-10 13:00:10 -0400

    NAS: use a much more aggressive timeout, which should speed up reconnects

--- kalnas_mount.sh
+++ kalnas_mount.sh
@@ -4,4 +4,4 @@ set -e
 
 # SSHFS wins over NFS: more responsive (ish), fewer moving parts,
 # easier set up, just as performant.
-sshfs -o ServerAliveInterval=30 -o reconnect kalnas:/mnt/Data/DataShare d
+sshfs -o reconnect,ServerAliveInterval=5,ServerAliveCountMax=2 kalnas:/mnt/Data/DataShare d/
